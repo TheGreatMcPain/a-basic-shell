6443c562b7679c22aea38e28c248a070c5398bc4
TheGreatMcPain <sixsupersonic@gmail.com>
2020-02-13 01:25:58 -0600

    Added separate function for the prompt which now has different colors, and displays the user, hostname, andm current directory. Added hints for some built-in commands. Added the ability to use escape character on spaces, so that cd can be used on directories with spaces. Some minor comments.

--- shell.c
+++ shell.c
@@ -39,12 +39,13 @@ struct Color {
 };
 
 // Prototypes
+void genPrompt(struct Color color1, struct Color color2);
 int isEmpty(char const *str);
 int isPosInteger(char const *str);
 int strToArgv(char *argv[], int size, char str[]);
 int lastCmd(char history[][MAX_LINE], char *str);
 void changeColor(char *const args[], struct Color colors[],
-                 struct Color *colorPtr);
+                 struct Color *colorPtr1, struct Color *colorPtr2);
 void changeDir(char *const args[]);
 pid_t execArgv(char *const args[]);
 
@@ -58,7 +59,8 @@ int main() {
   char *myArgv[ARGV_SIZE];
   char history[MAX_HIS][MAX_LINE];
   struct Color colors[NUM_COLORS];
-  struct Color color;
+  struct Color colorPrompt;
+  struct Color colorDir;
   int pids[MAX_PIDS];
   int i;
 
@@ -80,14 +82,16 @@ int main() {
 
   // Set default color.
   // '3' is blue.
-  color = colors[3];
+  colorPrompt = colors[1];
+  colorDir = colors[3];
 
   memset(pids, 0, sizeof(pids));
   memset(history, 0, sizeof(history));
 
   while (1) {
+    memset(cmd, 0, sizeof(cmd));
     /* Get User input */
-    printf("%s%s$ " RESET, color.ansi, getenv("PWD"));
+    genPrompt(colorPrompt, colorDir);
 
     // Error checking for 'fgets'.
     // This also makes sure gcc doesn't complain about fgets' return value.
@@ -130,7 +134,7 @@ int main() {
 
     /* Convert input to array of strings */
     if (strToArgv(myArgv, ARGV_SIZE, cmd) == 1) {
-      printf("Input must be less than, or equal to %d arguments.\n", ARGV_SIZE);
+      printf("Input must be less than %d arguments.\n", ARGV_SIZE);
       continue;
     }
 
@@ -139,7 +143,7 @@ int main() {
       printf("Exiting!\n");
       exit(0);
     } else if (strncmp(myArgv[0], "color", 5) == 0) {
-      changeColor(myArgv, colors, &color);
+      changeColor(myArgv, colors, &colorPrompt, &colorDir);
     } else if (strncmp(myArgv[0], "cd", 2) == 0) {
       changeDir(myArgv);
     } else if (strncmp(myArgv[0], "showpid", 7) == 0) {
@@ -182,19 +186,70 @@ int main() {
   return 0;
 }
 
+// Simply prints the 'prompt'
+void genPrompt(struct Color color1, struct Color color2) {
+  char *user = getlogin();
+  char *home = getenv("HOME");
+  char *currentDir = getenv("PWD");
+  char newDir[MAX_LINE];
+  char hostname[MAX_LINE];
+
+  // Grabs the hosts' hostname.
+  gethostname(hostname, MAX_LINE);
+
+  // Replace the home directory with '~'.
+  if (strncmp(currentDir, home, strlen(home)) == 0) {
+    strcpy(newDir, "~");
+    // This is setting the starting pointer of currentDir
+    // so that we skip the 'home' directory.
+    currentDir = currentDir + strlen(home);
+    strcat(newDir, currentDir);
+  } else {
+    // Otherwise just copy currentDir to newDir.
+    strcpy(newDir, currentDir);
+  }
+
+  // This should print "user@hostname current/directory >"
+  // where "user@hostname", and ">" will have their own color, and
+  // "current/directory" will have a different color.
+  printf("%s%s@%s %s%s%s > " RESET, color1.ansi, user, hostname, color2.ansi,
+         newDir, color1.ansi);
+}
+
 int strToArgv(char *argv[], int size, char str[]) {
   char tok[3] = " \n";
   char *token = NULL;
   int i = 0;
+  int x;
+  int y;
 
   token = strtok(str, tok);
 
   while (token != NULL) {
-    if (i == size) {
+    if (i == size - 1) {
       return 1;
     }
     argv[i] = token;
+
     token = strtok(NULL, tok);
+    i++;
+  }
+
+  // If one of the strings from argv have a '\'.
+  // Go replace '\' with a space, and append the next
+  // string(s). This is to allow 'cd' to work on
+  // directories with spaces in their name.
+  i = 0;
+  while (argv[i] != NULL) {
+    y = i;
+    while (argv[y][strlen(argv[y]) - 1] == '\\') {
+      argv[y][strlen(argv[y]) - 1] = '\0';
+      strcat(argv[y], " ");
+      strcat(argv[y], argv[y + 1]);
+      for (x = y + 1; argv[x] != NULL; x++) {
+        argv[x] = argv[x + 1];
+      }
+    }
     i++;
   }
   return 0;
@@ -234,7 +289,7 @@ int isPosInteger(char const *str) {
 // This function will return 1 if it whats to run a command,
 // but skip updating the history array.
 int lastCmd(char history[][MAX_LINE], char *str) {
-  char *args[2];
+  char *args[3];
   char newCmd[MAX_LINE];
   int i;
   int num;
@@ -248,7 +303,7 @@ int lastCmd(char history[][MAX_LINE], char *str) {
   memset(selection, 0, sizeof(selection));
   memset(args, 0, sizeof(args));
   // convert the copy of str to args array.
-  strToArgv(args, 2, newCmd);
+  strToArgv(args, 3, newCmd);
 
   // This doesn't actually forkbomb I swear!
   if (strncmp(args[0], "forkbomb", 8) == 0) {
@@ -300,17 +355,19 @@ int lastCmd(char history[][MAX_LINE], char *str) {
       }
       // Don't attempt to access something from 'history' that
       // doesn't exist.
-      if (num >= MAX_HIS + 1) {
-        printf("\"%d\" is an invalid option.\n", num);
-        return 2;
-      } else if (selection[num] == 0) {
-        printf("\"%d\" is an invalid option.\n", num);
+      if (num >= MAX_HIS + 1 || selection[num] == 0) {
+        printf("\"%d\" Doesn't exist in history list.\n\n", num);
+        printf("Use 'lc list' to list command history.\n\n");
         return 2;
       }
+
       strcpy(str, history[selection[num]]);
       return 1;
     } else if (args[1] != NULL) {
-      printf("\"%s\" is not a valid option.\n", args[1]);
+      printf("\"%s\" is not a valid option.\n\n", args[1]);
+      printf("Use 'lc' to run the previous command.\n\n");
+      printf("'lc list' to list command history starting with the latest.\n\n");
+      printf("'lc <# from 'lc list'>' to run commands from the history.\n\n");
       return 2;
     }
   }
@@ -327,29 +384,53 @@ int lastCmd(char history[][MAX_LINE], char *str) {
 // If the name of a color is provided it will change the value of
 // colorPtr to that color.
 void changeColor(char *const args[], struct Color colors[],
-                 struct Color *colorPtr) {
+                 struct Color *colorPtr1, struct Color *colorPtr2) {
+  struct Color *colorTmpPtr = NULL;
   int i;
 
   // If no arguments print the current color.
   if (args[1] == NULL) {
-    printf("Current color is: %s%s\n" RESET, colorPtr->ansi, colorPtr->name);
+    printf("Current prompt color is: %s%s\n" RESET, colorPtr1->ansi,
+           colorPtr1->name);
+    printf("Current directory color is: %s%s\n" RESET, colorPtr2->ansi,
+           colorPtr2->name);
     // If 'list' is supplied list the available colors.
   } else if (strncmp(args[1], "list", 4) == 0) {
     for (i = 0; i < NUM_COLORS; i++) {
       printf("%s%s\n", colors[i].ansi, colors[i].name);
     }
-    // If a color name is supplied. Than search through the
-    // colors array and set 'color' if found. Otherwise print
-    // error message.
+    // if 'prompt' is supplied change the 'colorTmpPtr' pointer
+    // to colorPtr1. This way when we change the value of 'colorTmpPtr'
+    // we also change the value that colorPtr1 points to.
+  } else if (strncmp(args[1], "prompt", 6) == 0) {
+    colorTmpPtr = colorPtr1;
+    // Same as before, but for colorPtr2.
+  } else if (strncmp(args[1], "dir", 3) == 0) {
+    colorTmpPtr = colorPtr2;
+    // Tell the user how to use this command.
   } else if (strncmp(args[1], "\0", 1) != 0) {
-    for (i = 0; i < NUM_COLORS; i++) {
-      if (strcmp(colors[i].name, args[1]) == 0) {
-        *colorPtr = colors[i];
-        break;
+    printf("\"%s\" Is not a valid option.\n\n", args[1]);
+
+    printf("Use 'color' to show the currently selected colors.\n\n");
+    printf("'color list' to list available colors.\n\n");
+    printf("'color prompt <color>' will change the color of the prompt.\n\n");
+    printf("'color dir <color>' will change the color of current directory");
+    printf("part of the prompt.\n\n");
+  }
+
+  if (colorTmpPtr != NULL) {
+    if (args[2] == NULL) {
+      printf("You need to input a color.\n");
+    } else {
+      for (i = 0; i < NUM_COLORS; i++) {
+        if (strcmp(colors[i].name, args[2]) == 0) {
+          *colorTmpPtr = colors[i];
+          break;
+        }
       }
-    }
-    if (i == NUM_COLORS) {
-      printf("\"%s\" Is not a color.\n", args[1]);
+      if (i == NUM_COLORS) {
+        printf("\"%s\" Is not a color.\n", args[2]);
+      }
     }
   }
 }
@@ -399,6 +480,11 @@ void changeDir(char *const args[]) {
 // that returns the pid from execvp.
 // Hopefully this will make 'showpid' easier to
 // implement.
+//
+// Update 02-06-2020: So I learned how to use pipes
+// so that the child process can pass information back to
+// the parent process. In this case I'm sending the return value
+// to the parent which will help the parent know if execvp fails.
 pid_t execArgv(char *const args[]) {
   pid_t pid;
   // Used for return value of execvp.
